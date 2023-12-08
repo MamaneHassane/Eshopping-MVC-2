1c7a59792af94ec0b941e4a7fdf00ef9f0b8c359
MamaneHassane <hassane.mamane017@gmail.com>
2023-12-08 10:29:40 +0100

    10:29

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+using System.Security.Claims;
+using Eshopping_MVC.Data;
+using Eshopping_MVC.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshopping_MVC.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly AppDbContext _context;
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(AppDbContext context, ILogger<AuthController> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+    [HttpGet]
+    public IActionResult Login()
+    {
+        ClaimsPrincipal claimUser = HttpContext.User;
+        if(claimUser.Identity.IsAuthenticated)
+            return RedirectToAction("Index", "Home");
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Login(Client Client)
+    {
+        if (Client.email == "" && Client.password == "")
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,Client.Login),
+                new Claim("Role","User")
+            };
+            ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+                AllowRefresh = true,
+                IsPersistent = true
+            };
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), properties);
+            return RedirectToAction("Index", "Home");
+        }
+        return View();
+    }
+}
